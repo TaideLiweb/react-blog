a5d792606f1cc02d73ddc51bed5d372721ad0753
TaideLiweb <teddy55987@gmail.com>
2022-07-31 15:10:46 +0800

    新增送出文章後 清除input

--- src/EditPage.jsx
+++ src/EditPage.jsx
@@ -7,30 +7,47 @@ import { getDatabase,ref,push,onValue} from "firebase/database";
 
 
 function EditPage() {
+
     const [PostContent,setPostContent] = useState('')
+    const [Postkey,setPostkey] = useState(0)
     const [PostTitle,setPostTitle] = useState('')
     // Initialize Firebase
     const database = getDatabase(firebase);
-    const dbRef = ref(database);
     
     // onValue(dbRef, (snapshot) => {
     //     const data = snapshot.val();
     //     console.log(data)
     // });
     
-    function writeUserData() {
-        push(ref(database,), {
-            postTitle:PostTitle,
-            postContent:PostContent
-        });
+    function submitPost() {
+        // push(ref(database), {
+        //     postTitle:PostTitle,
+        //     postContent:PostContent
+        // });
+
+        //初始化CKediter編輯器
+        setPostkey(Postkey+1)
+        setPostTitle('')
     }
 
     return (
         <div className="container px-4 px-lg-5">
             <div className="row gx-4 gx-lg-5 justify-content-center">
                 <div className="col-md-10 col-lg-8 col-xl-7">
-                    <input type="text" onChange={(e)=> setPostTitle(e.target.value)} onBlur={(e)=> setPostTitle(e.target.value)} />
+                <div className='d-flex align-items-center mb-3'>
+                    <span>文章標題：</span>
+                    <div className="input-outline-x">
+                        <input className="input-control input-outline" placeholder="標題" type="text" value={PostTitle} onChange={(e)=> setPostTitle(e.target.value)} onBlur={(e)=> setPostTitle(e.target.value)} />
+                        <label className="input-label">標題</label>
+                    </div>
+                </div>
+                <div className='mb-3'>
                     <CKEditor
+                    key={Postkey}
+                    // config={{
+                    //     filebrowserBrowseUrl: 'gs://blog-7078e.appspot.com',
+                    //     filebrowserUploadUrl: 'gs://blog-7078e.appspot.com'
+                    // }}
                     onChange={ ( { editor } ) => {  
                         setPostContent(editor.getData())
                     } }
@@ -40,19 +57,19 @@ function EditPage() {
                         }
                     }
                     />
-                    <div>
+                </div>
+                    {/* <div>
                         標題:{PostTitle}
                     </div>
                     <div>
                         
                         {PostContent}
+                    </div> */}
+                    <div className="d-flex justify-content-end mb-4">
+                        <button className="btn btn-primary text-uppercase"  onClick={()=>{
+                        submitPost()
+                    }}>送出</button>
                     </div>
-                    <div>
-                        {/* {Test} */}
-                    </div>
-                    <button onClick={()=>{
-                        writeUserData()
-                    }}>點我</button>
                 </div>
             </div>
         </div>
