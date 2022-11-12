07cd8a2ee9b64d1aad21e8269b245a73b9a3476f
TaideLiweb <teddy55987@gmail.com>
2022-11-12 17:24:40 +0800

    修正並調整首頁RWD

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,8 +1,8 @@
 // import Background from 'public/home-bg.jpg';
-
+import background from "../753053.jpg";
 function Header() {
     return (
-        <header className="masthead" style={{backgroundImage: `url('753053.jpg')`}}>
+        <header className="masthead" style={{backgroundImage: `url(${background})`}}>
             <div className="container position-relative px-4 px-lg-5">
                 <div className="row gx-4 gx-lg-5 justify-content-center">
                     <div className="col-md-10 col-lg-8 col-xl-7">
@@ -17,4 +17,4 @@ function Header() {
     );
 }
 
-export default Header;
+export default Header;  
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.scss';
 import './css/templateStyle.scss';
+import './css/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
